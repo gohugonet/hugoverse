1ea1763619d609207cfa13234e7bbb3b8bf4a9ea
sunwei <me@sunwei.xyz>
2024-03-02 11:01:24 +0800

    comand line init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gohugonet/hugoverse
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gohugonet/hugoverse/cmd"
+	"log"
+)
+
+func main() {
+	log.SetFlags(0)
+	err := cmd.New()
+	if err != nil {
+		log.Fatalf("\nError: %s", err)
+	}
+}
